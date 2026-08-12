ac734dc1b496c55836179814d52c0577a47b006c
thomazguimaraes21 <thomazguimaraes0@gmail.com>
2026-08-12 13:30:31 -0300

    reformata tupla desafio 73, multilinha

--- desafio73.py
+++ desafio73.py
@@ -1,7 +1,10 @@
 print('CAMPEONATO BRASILEIRO')
-a = ('Palmeiras','Flamengo','Athletico-PR','Fluminense','Cruzeiro','Bahia','Corinthians','Bragantino','Botafogo','Coritiba','Atlético Mineiro','São Paulo','Vitoria','Gremio','Mirassol','Internacional','Santos','Vasco','Remo','Chapecoense')
-b = print(a[0:5])
-c = print(a[16:20])
-d = print(sorted(a))
-e = print(a.index('Chapecoense'))
-print('Corinthians maior do Brasil!!!!!!!!!!')
+a = ('Palmeiras','Flamengo','Athletico-PR','Fluminense',
+     'Cruzeiro','Bahia','Corinthians',
+     'Bragantino','Botafogo','Coritiba','Atlético Mineiro','São Paulo',
+     'Vitoria','Gremio','Mirassol','Internacional',
+     'Santos','Vasco','Remo','Chapecoense')
+b = print(a[0:5]) # mostra os 5 primeiros colocados
+c = print(a[16:20]) # mostra os 4 ultimos
+d = print(sorted(a)) # coloca em ordem alfabética
+e = print(a.index('Chapecoense')) # mostra a pos da chapecoense
